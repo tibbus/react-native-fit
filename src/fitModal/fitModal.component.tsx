7706e4d09c1ca2d3ad150534cd9e5eea327a0241
tibs <ttiberiup@gmail.com>
2017-06-24 12:57:06 +0300

    add package devDependecies
    * remove unused Icon lib

--- src/fitModal/fitModal.component.tsx
+++ src/fitModal/fitModal.component.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Modal, Text, TouchableOpacity, View, ViewStyle, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 import { FitModalProps } from './fitModal.props';
 import { styles } from './fitModal.styles';
